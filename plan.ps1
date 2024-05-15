4dc1c8b25728448099e6471efd15d7667e7e3a32
jesusqm <tic-jquispe@cajalosandes.pe>
2024-05-15 01:08:15 -0500

    change parameters module vpc

--- plan.ps1
+++ plan.ps1
@@ -2,7 +2,7 @@ $AWS_ACCESS_KEY = Read-Host -Prompt "> AWS ACCESS KEY :"
 $AWS_SECRET_KEY = Read-Host -AsSecureString -Prompt "> Complete Secret key to '$AWS_ACCESS_KEY '"
 $plainPassword = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto([System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($AWS_SECRET_KEY))
 
-$command = "terraform plan -var='iac_aws_region=us-east-1' -var='iac_aws_access_key=$AWS_ACCESS_KEY' -var='iac_aws_secret_key=$plainPassword' -var='aws_project_id=iacdevops' -var='aws_project_env=develop' -var='aws_project_account=integrat-dev' -var='aws_account_origin=devops' -var='aws_account_target=integrations-dev'"
+$command = "terraform plan -var='iac_aws_region=us-east-1' -var='iac_aws_access_key=$AWS_ACCESS_KEY' -var='iac_aws_secret_key=$plainPassword' -var='aws_project_id=Enviroment' -var='aws_project_env=dev' -var='aws_project_account=integrat-dev' -var='aws_account_origin=devops' -var='aws_account_target=integrations-dev'"
 Write-Host "> Applicando cambios terraform"
 Invoke-Expression $command
 Write-Host "> Ejecución terminado..."
